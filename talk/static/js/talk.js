356a8596b72b620c3d6822355ca2e8a988bf5431
Hannes hermansson <hanneshermansson0000@gmail.com>
2017-08-06 19:00:29 +0200

    added js ui functions/ minor ui improvements

---
+++ talk/static/js/talk.js
@@ -0,0 +1,32 @@
+
+window.onload = function(){
+let arr = [
+"#f1c40f", //yellow
+"#1abc9c", //turq
+"#3498db", //blue
+"#27ae60", //green
+"#e74c3c", //red
+"#9b59b6" //purple
+];
+let clr = arr[Math.floor(Math.random() * arr.length)];
+$(".article-title").css("background-color", clr);
+
+$( ".catsw" ).click(function() {
+
+  let am = $(".amenu-sub");
+  let ami = $(".catsw i");
+
+  if(am.css("display") == "flex"){
+    am.hide();
+    ami.css("transform","rotate(0deg)")
+  }
+  else{
+    am.css("display","flex");
+    ami.css("transform","rotate(90deg)")
+  }
+
+
+});
+
+
+}
